f3eba4826b9709cc5d13c45cc40ac089f528b692
Gert9929 <gert-arne.rattasepp@khk.ee>
2023-01-09 14:22:02 +0200

    skirpt

--- lisa_kasutaja.sh
+++ lisa_kasutaja.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+#Küsib userilt nime
 echo -n "Sisesta kasutajanimi: "
 read nimi
+
 #echo -n "Sisesta parool: "
 #stty -echo
 #read parool
 echo
+#Loob kasutaja
 useradd $nimi # -p $parool
+
+#Teeb uue home kataloogi
 mkdir /home/$nimi 
+
+#useradd'ile tuleb manuaalselt lisada kõik sätted ja õigused samal ajal kui adduser teeb kõik automaatselt
