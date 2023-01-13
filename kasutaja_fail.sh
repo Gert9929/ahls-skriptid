734f8af8f4c8024e43fc9c37383343357ceb770a
Gert9929 <gert-arne.rattasepp@khk.ee>
2023-01-13 13:46:49 +0200

    Kasutajate lisamine

---
+++ kasutaja_fail.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "Kasutusjuhend: $0 failinimi"
+else
+	failinimi=$1
+	if [ -f $failinimi -a -r $failinimi ]; then
+		echo "fail on korras"
+		for nimi in $(cat $failinimi)
+		do
+			sh lisa_kasutaja.sh $nimi
+		done
+	else
+		echo "probleem failiga $failinimi"
+
+	fi
+fi
+
--- lisa_kasutaja.sh
+++ lisa_kasutaja.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
+if [ $# -ne 1 ]; then
+	echo "Kasutusjuhend: $0 kasutajanimi"
+else
+	kasutajanimi=$1
+	useradd $kasutajanimi -m -s /bin/bash
 
-#Küsib userilt nime
-echo -n "Sisesta kasutajanimi: "
-read nimi
-
-#echo -n "Sisesta parool: "
-#stty -echo
-#read parool
-echo
-#Loob kasutaja
-useradd $nimi # -p $parool
-
-#Teeb uue home kataloogi
-mkdir /home/$nimi 
-
-#useradd'ile tuleb manuaalselt lisada kõik sätted ja õigused samal ajal kui adduser teeb kõik automaatselt
+	if [ $? -eq 0 ]; then
+		echo "Kasutaja nimega $kasutajanimi  on lisatud süsteemi"
+		cat /etc/passwd | grep $kasutajanimi
+		ls -la /home/$kasutajanimi
+	else
+		echo "Probleem kasutaja $kasutajanimi lisamisega"
+		echo "Probleem kood on $?"
+	fi
+fi
